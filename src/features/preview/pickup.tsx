a7c4db8e2208aa921cb8a1e727dc2d5378a8bff4
shindongri <tlsehdfl@naver.com>
2021-05-14 16:43:47 +0900

    .

--- src/features/preview/pickup.tsx
+++ src/features/preview/pickup.tsx
@@ -9,17 +9,49 @@ interface StepProps {
 const Step: FC<StepProps> = ({ imageUrl, title, subtitle }) => {
   return (
     <div
-      style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "fit-content", minWidth: "80px", height: "100%" }}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "fit-content",
+        minWidth: "80px",
+        height: "100%",
+      }}
     >
-      <div style={{ width: "auto", minWidth: "45px", height: "60px", objectFit: "contain" }}>
+      <div
+        style={{
+          width: "auto",
+          minWidth: "45px",
+          height: "60px",
+          objectFit: "contain",
+        }}
+      >
         <img src={imageUrl} alt="" />
       </div>
       <h6
-        style={{ fontSize: "10px", fontWeight: "bold", padding: 0, marginTop: "16px" }}
+        style={{
+          fontSize: "10px",
+          fontWeight: "bold",
+          padding: 0,
+          marginTop: "16px",
+        }}
       >
         {title}
       </h6>
-      <p style={{ fontSize: "10px", padding: "4px 0 0 0", margin: 0, lineHeight: 1.5, whiteSpace: "pre-line", textAlign: "center" }}>{subtitle}</p>
+      {subtitle.split("\n").map((s, i) => (
+        <p
+          style={{
+            fontSize: "10px",
+            padding:  i === 0 ? "4px 0 0 0" : 0,
+            margin: 0,
+            lineHeight: 1.5,
+            whiteSpace: "nowrap",
+            textAlign: "center",
+          }}
+        >
+          {s}
+        </p>
+      ))}
     </div>
   );
 };
@@ -32,13 +64,28 @@ const PickupFragment: FC = () => {
         height: "248px",
         backgroundColor: "#cddf74",
         padding: "30px 1.5rem",
-        boxSizing: "border-box"
+        boxSizing: "border-box",
       }}
     >
-      <h2 style={{ fontSize: "18px", fontWeight: "bold", margin: "0px", paddingBottom: "18px" }}>
+      <h2
+        style={{
+          fontSize: "18px",
+          fontWeight: "bold",
+          margin: "0px",
+          paddingBottom: "18px",
+        }}
+      >
         렛츠와인 페어링 픽업 안내
       </h2>
-      <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "100%", height: "100%" }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "100%",
+          height: "100%",
+        }}
+      >
         <Step
           imageUrl="http://wineplz.cafe24.com/web/upload/img/icon_wine02.svg"
           title="평일 오전 10시"
