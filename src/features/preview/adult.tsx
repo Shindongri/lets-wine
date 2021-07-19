274fc4be0844a8cdfe68f1465803fd1d592ba672
shindongri <tlsehdfl@naver.com>
2021-07-19 09:18:47 +0900

    .

--- src/features/preview/adult.tsx
+++ src/features/preview/adult.tsx
@@ -3,137 +3,137 @@ import { FC } from "react";
 const AdultFragment: FC = () => {
   return (
     <div style={{ width: "100%", marginBottom: "1rem" }}>
-      <div
-        style={{
-          backgroundColor: "#f1f1f1",
-          width: "100%",
-          height: "5rem",
-          fontSize: "1rem",
-          lineHeight: 1.5,
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "10px 1.5rem",
-          boxSizing: "border-box",
-          position: "relative",
-        }}
-      >
-        <div
-          style={{
-            width: "65%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-          }}
-        >
-          <p style={{ fontSize: "12px", margin: "0px", padding: "0px" }}>
-            회원가입 시 성인인증을 완료했다면 -
-          </p>
-          <div
-            style={{
-              display: "block",
-              textAlign: "center",
-              padding: "6px 10px",
-              border: "1px solid rgb(218, 218, 218)",
-              borderRadius: "30px",
-              fontSize: "12px",
-              width: "100px",
-            }}
-          >
-            계속 이용하기
-          </div>
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            width: "7rem",
-            right: "1.25rem",
-            bottom: "-8px",
-          }}
-        >
-          <img
-            style={{
-              objectFit: "contain",
-              width: "100%",
-              height: "100%",
-            }}
-            src="https://wineplz.cafe24.com/web/upload/img/pro_ok3.svg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div
-        style={{
-          width: "100%",
-          height: "1px",
-          backgroundColor: "rgb(218, 218, 218)",
-        }}
-      />
-      <div
-        style={{
-          width: "100%",
-          height: "5rem",
-          fontSize: "1rem",
-          lineHeight: 1.5,
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          padding: "10px 1.5rem",
-          boxSizing: "border-box",
-          position: "relative",
-          backgroundColor: "rgb(241, 241, 241)",
-        }}
-      >
-        <div
-          style={{
-            width: "65%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-          }}
-        >
-          <p style={{ fontSize: "12px", margin: "0px", padding: "0px" }}>
-            회원가입 시{' '}
-            <b style={{ fontWeight: "bold" }}>성인인증을 하지 않았다면 -</b>
-          </p>
-          <a
-            style={{
-              display: "block",
-              textAlign: "center",
-              padding: "6px 10px",
-              color: "rgb(255, 255, 255)",
-              backgroundColor: "rgb(31, 41, 55)",
-              textDecoration: "none",
-              borderRadius: "30px",
-              fontSize: "12px",
-              width: "100px",
-            }}
-            href="/"
-          >
-            인증하러 가기
-          </a>
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            width: "4rem",
-            bottom: "-8px",
-            right: "2rem",
-          }}
-        >
-          <img
-            style={{
-              objectFit: "contain",
-              width: "100%",
-              height: "100%",
-            }}
-            src="https://wineplz.cafe24.com/web/upload/img/pro_go3.svg"
-            alt=""
-          />
-        </div>
-      </div>
+      {/*<div*/}
+      {/*  style={{*/}
+      {/*    backgroundColor: "#f1f1f1",*/}
+      {/*    width: "100%",*/}
+      {/*    height: "5rem",*/}
+      {/*    fontSize: "1rem",*/}
+      {/*    lineHeight: 1.5,*/}
+      {/*    display: "flex",*/}
+      {/*    justifyContent: "space-between",*/}
+      {/*    alignItems: "center",*/}
+      {/*    padding: "10px 1.5rem",*/}
+      {/*    boxSizing: "border-box",*/}
+      {/*    position: "relative",*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  <div*/}
+      {/*    style={{*/}
+      {/*      width: "65%",*/}
+      {/*      height: "100%",*/}
+      {/*      display: "flex",*/}
+      {/*      flexDirection: "column",*/}
+      {/*      justifyContent: "space-between",*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    <p style={{ fontSize: "12px", margin: "0px", padding: "0px" }}>*/}
+      {/*      회원가입 시 성인인증을 완료했다면 -*/}
+      {/*    </p>*/}
+      {/*    <div*/}
+      {/*      style={{*/}
+      {/*        display: "block",*/}
+      {/*        textAlign: "center",*/}
+      {/*        padding: "6px 10px",*/}
+      {/*        border: "1px solid rgb(218, 218, 218)",*/}
+      {/*        borderRadius: "30px",*/}
+      {/*        fontSize: "12px",*/}
+      {/*        width: "100px",*/}
+      {/*      }}*/}
+      {/*    >*/}
+      {/*      계속 이용하기*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*  <div*/}
+      {/*    style={{*/}
+      {/*      position: "absolute",*/}
+      {/*      width: "7rem",*/}
+      {/*      right: "1.25rem",*/}
+      {/*      bottom: "-8px",*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    <img*/}
+      {/*      style={{*/}
+      {/*        objectFit: "contain",*/}
+      {/*        width: "100%",*/}
+      {/*        height: "100%",*/}
+      {/*      }}*/}
+      {/*      src="https://wineplz.cafe24.com/web/upload/img/pro_ok3.svg"*/}
+      {/*      alt=""*/}
+      {/*    />*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+      {/*<div*/}
+      {/*  style={{*/}
+      {/*    width: "100%",*/}
+      {/*    height: "1px",*/}
+      {/*    backgroundColor: "rgb(218, 218, 218)",*/}
+      {/*  }}*/}
+      {/*/>*/}
+      {/*<div*/}
+      {/*  style={{*/}
+      {/*    width: "100%",*/}
+      {/*    height: "5rem",*/}
+      {/*    fontSize: "1rem",*/}
+      {/*    lineHeight: 1.5,*/}
+      {/*    display: "flex",*/}
+      {/*    justifyContent: "space-between",*/}
+      {/*    alignItems: "center",*/}
+      {/*    padding: "10px 1.5rem",*/}
+      {/*    boxSizing: "border-box",*/}
+      {/*    position: "relative",*/}
+      {/*    backgroundColor: "rgb(241, 241, 241)",*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  <div*/}
+      {/*    style={{*/}
+      {/*      width: "65%",*/}
+      {/*      height: "100%",*/}
+      {/*      display: "flex",*/}
+      {/*      flexDirection: "column",*/}
+      {/*      justifyContent: "space-between",*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    <p style={{ fontSize: "12px", margin: "0px", padding: "0px" }}>*/}
+      {/*      회원가입 시{' '}*/}
+      {/*      <b style={{ fontWeight: "bold" }}>성인인증을 하지 않았다면 -</b>*/}
+      {/*    </p>*/}
+      {/*    <a*/}
+      {/*      style={{*/}
+      {/*        display: "block",*/}
+      {/*        textAlign: "center",*/}
+      {/*        padding: "6px 10px",*/}
+      {/*        color: "rgb(255, 255, 255)",*/}
+      {/*        backgroundColor: "rgb(31, 41, 55)",*/}
+      {/*        textDecoration: "none",*/}
+      {/*        borderRadius: "30px",*/}
+      {/*        fontSize: "12px",*/}
+      {/*        width: "100px",*/}
+      {/*      }}*/}
+      {/*      href="/"*/}
+      {/*    >*/}
+      {/*      인증하러 가기*/}
+      {/*    </a>*/}
+      {/*  </div>*/}
+      {/*  <div*/}
+      {/*    style={{*/}
+      {/*      position: "absolute",*/}
+      {/*      width: "4rem",*/}
+      {/*      bottom: "-8px",*/}
+      {/*      right: "2rem",*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    <img*/}
+      {/*      style={{*/}
+      {/*        objectFit: "contain",*/}
+      {/*        width: "100%",*/}
+      {/*        height: "100%",*/}
+      {/*      }}*/}
+      {/*      src="https://wineplz.cafe24.com/web/upload/img/pro_go3.svg"*/}
+      {/*      alt=""*/}
+      {/*    />*/}
+      {/*  </div>*/}
+      {/*</div>*/}
       <div
         style={{
           width: "100%",
